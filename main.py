8f43a5ce3608ab718105136428e9df04b65fbb9d
BugSpace <409478@niuitmo.ru>
2026-01-31 17:49:11 +0700

    add the ability to enter text in fields. Now you can click on the fields and enter the numbers.

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ GREEN = (60, 179, 113)
 RED = (220, 20, 60)
 
 font = pygame.font.Font(None, 40)
+font_digits = pygame.font.Font(None, 80)
 
 # Поле для рублей
 rubles_rect = pygame.Rect(screen_width * 4 // 9, screen_width // 9, screen_width * 4 // 9, screen_width // 6)
@@ -23,11 +24,43 @@ rate_rect = pygame.Rect(screen_width * 4 // 9, screen_width * 5 // 9, screen_wid
 # Кнопка конвертации
 button_rect = pygame.Rect(screen_width * 2 // 9, screen_width * 3 // 9, screen_width * 5 // 9, screen_width // 6)
 
+# Данные
+rubles_text = ""  # Текст в поле рублей
+rate_text = "90"  # Текст в поле курса (начальный курс 90)
+active_field = None
+
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_pos = pygame.mouse.get_pos()
+
+            # Проверяем, куда кликнули
+            if rubles_rect.collidepoint(mouse_pos):
+                active_field = "rubles"
+            elif rate_rect.collidepoint(mouse_pos):
+                active_field = "rate"
+            else:
+                active_field = None
+
+            # Обработка нажатия клавиш
+        elif event.type == pygame.KEYDOWN:
+            if active_field == "rubles":
+                if event.key == pygame.K_BACKSPACE:
+                    rubles_text = rubles_text[:-1]  # Удаляем последний символ
+                else:
+                    # Добавляем только цифры или точку
+                    if event.unicode.isdigit() or event.unicode == ".":
+                        rubles_text += event.unicode
+
+            elif active_field == "rate":
+                if event.key == pygame.K_BACKSPACE:
+                    rate_text = rate_text[:-1]
+                else:
+                    if event.unicode.isdigit() or event.unicode == ".":
+                        rate_text += event.unicode
 
     screen.fill(BLACK)
     # Рисуем поля ввода
@@ -51,6 +84,12 @@ while running:
     # Текст на кнопке
     button_text = font.render("Конвертировать", True, WHITE)
     screen.blit(button_text, (screen_width * 2 // 9 + 55, screen_width * 3 // 9 + 35))
+
+    rubles_display = font_digits.render(rubles_text if rubles_text else "0", True, WHITE)
+    screen.blit(rubles_display, (rubles_rect.x + 15, rubles_rect.y + 20))
+
+    rate_display = font_digits.render(rate_text, True, WHITE)
+    screen.blit(rate_display, (rate_rect.x + 15, rate_rect.y + 20))
 
     pygame.display.flip()
 
